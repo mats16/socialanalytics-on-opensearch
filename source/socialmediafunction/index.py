f7273e0bd61d7711fe3c14d99647e3939eb49f90
Kazuki Matsuda <mazda@amazon.com>
2019-11-20 20:58:26 +0900

    add normalize

--- source/socialmediafunction/index.py
+++ source/socialmediafunction/index.py
@@ -2,15 +2,24 @@ import json
 import boto3
 import os
 from bs4 import BeautifulSoup
+import neologdn
+import re
 
 from aws_xray_sdk.core import patch
-
 patch(['boto3'])
+
+entity_score_hreshold = 0.7
 
 s3 = boto3.resource('s3')
 comprehend = boto3.client('comprehend')
 translate = boto3.client('translate')
 firehose = boto3.client('firehose')
+
+def normalize(text):
+    text = neologdn.normalize(text)
+    text = re.sub(r'https?(:\/\/[-_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+\$,%#]+)', '', text)  # remove URL
+    #text = re.sub(r'@[a-zA-Z0-9_]+', '', text)  # remove twitter_account
+    return text
 
 def lambda_handler(event, context):
     print(event)
@@ -20,16 +29,14 @@ def lambda_handler(event, context):
         s3_key = record['s3']['object']['key']
         
         obj = s3.Object(s3_bucket, s3_key)
-        tweets_as_string = obj.get()['Body'].read().decode('utf-8') 
-        
-        tweets = tweets_as_string.split('\n')
+        tweets = obj.get()['Body'].read().decode('utf-8').split('\n')
         status = {'tweet': 0, 'retweet': 0, 'quote': 0}
         for tweet_string in tweets:
             
             if len(tweet_string) < 1:
                 continue
-            
-            tweet = json.loads(tweet_string)
+            else:
+                tweet = json.loads(tweet_string)
             if 'retweeted_status' in tweet:
                 status['retweet'] += 1
                 continue
@@ -39,7 +46,7 @@ def lambda_handler(event, context):
                 status['tweet'] += 1
             
             if tweet['lang'] in ['en', 'es', 'fr', 'de', 'it', 'pt', 'ar', 'hi', 'ja', 'ko', 'zh']:
-                comprehend_text = tweet['text']
+                comprehend_text = normalize(tweet['text'])
                 comprehend_lang = tweet['lang']
             else:
                 response = translate.translate_text(
@@ -53,14 +60,6 @@ def lambda_handler(event, context):
                 Text=comprehend_text,
                 LanguageCode=comprehend_lang
             )
-            #print(sentiment_response)
-            
-            entities_response = comprehend.detect_entities(
-                Text=comprehend_text,
-                LanguageCode=comprehend_lang
-            )
-            #print(entities_response)
-
             sentiment_record = {
                 'tweetid': tweet['id_str'],
                 'text': comprehend_text,
@@ -71,14 +70,18 @@ def lambda_handler(event, context):
                 'sentimentneuscore': sentiment_response['SentimentScore']['Neutral'],
                 'sentimentmixedscore': sentiment_response['SentimentScore']['Mixed']
             }
-            
             response = firehose.put_record(
                 DeliveryStreamName=os.environ['SENTIMENT_STREAM'],
                 Record={
                     'Data': json.dumps(sentiment_record) + '\n'
                 }
             )
-            
+
+            entities_response = comprehend.detect_entities(
+                Text=comprehend_text,
+                LanguageCode=comprehend_lang
+            )
+            print(entities_response)
             seen_entities = []
             for entity in entities_response['Entities']:
                 id = entity['Text'] + '-' + entity['Type']
@@ -109,7 +112,6 @@ def lambda_handler(event, context):
                 'filter_level': tweet['filter_level'],
                 'timestamp': tweet['timestamp_ms'],
                 'lang': tweet['lang'],
-                'hashtags': [hashtag['text'] for hashtag in tweet['entities']['hashtags']],
                 'url': 'https://twitter.com/{0}/status/{1}'.format(tweet['user']['screen_name'], tweet['id_str']),  
                 'comprehend': { 
                     'text': comprehend_text,    
@@ -120,13 +122,33 @@ def lambda_handler(event, context):
                         'negative': sentiment_response['SentimentScore']['Negative'],
                         'neutral': sentiment_response['SentimentScore']['Neutral'],
                         'mixed': sentiment_response['SentimentScore']['Mixed'],
-                    },
-                    'entities': [entity['Text'] for entity in entities_response['Entities']],
-                    'key_phrases': [i['Text'] for i in key_phrases_response['KeyPhrases']],
+                    }
                 }
             }
+            entities = []
+            for entity in entities_response['Entities']:
+                if entity['Type'] in ['QUANTITY', 'DATE', 'PERSON']:
+                    continue
+                elif len(entity['Text']) < 2:
+                    continue
+                elif entity['Score'] >= entity_score_hreshold:
+                    entities.append(entity['Text'])
+            if len(entities) > 0:
+                enriched_record['comprehend']['entities'] = list(set(entities))
+
+            key_phrases = []
+            for key_phrase in key_phrases_response['KeyPhrases']:
+                key_phrases.append(key_phrase['Text'])
+            if len(key_phrases) > 0:
+                enriched_record['comprehend']['key_phrases'] = list(set(key_phrases))
+
+            hashtags = [hashtag['text'] for hashtag in tweet['entities']['hashtags']]
+            if len(hashtags) > 0:
+                enriched_record['hashtags'] = hashtags
+
             if 'coordinates' in tweet:
                 enriched_record['coordinates'] = tweet['coordinates']
+
             response = firehose.put_record(
                 DeliveryStreamName=os.environ['ANALYTICS_STREAM'],
                 Record={
