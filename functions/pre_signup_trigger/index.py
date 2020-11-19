0e3d418b78916470734f27bd3cacf08778e51e8a
Kazuki Matsuda <mazda@amazon.co.jp>
2020-11-19 12:12:37 +0900

    add labeling ui to output

--- functions/pre_signup_trigger/index.py
+++ functions/pre_signup_trigger/index.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-import logging
 import os
+from aws_lambda_powertools import Logger
 
 allowed_domains = os.environ.get('ALLOWED_DOMAINS')
-logger = logging.getLogger(__name__)
+logger = Logger()
 
 def lambda_handler(event, context):
+    logger.info(event)
     email = event['request']['userAttributes']['email']
     domain = email.split('@')[1]
     if allowed_domains == '*':
@@ -15,5 +16,4 @@ def lambda_handler(event, context):
         pass
     else:
         raise Exception('@{0} is not allowed to regiter.'.format(domain))
-    # Return to Amazon Cognito
     return event
