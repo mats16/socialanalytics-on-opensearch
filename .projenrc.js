db792353b31445c618211abb6ec2a48dd07e7f95
mats16 <mats.kazuki@gmail.com>
2022-01-11 15:40:01 +0900

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,14 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkTypeScriptApp({
+  cdkVersion: '1.129.0',
+  defaultReleaseBranch: 'main',
+  name: 'serverless-social-media-dashboard',
+
+  // cdkDependencies: undefined,  /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
+  // deps: [],                    /* Runtime dependencies of this module. */
+  // description: undefined,      /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],                 /* Build dependencies for this module. */
+  // packageName: undefined,      /* The "name" in package.json. */
+  // release: undefined,          /* Add release management to this project. */
+});
+project.synth();
