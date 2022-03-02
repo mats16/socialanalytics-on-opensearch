3ba74a2fb39baf97ef0519672f36324a4ef25627
mats16 <mats.kazuki@gmail.com>
2022-03-02 17:09:03 +0900

    update cdk version

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,6 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.14.0',
+  cdkVersion: '2.15.0',
   defaultReleaseBranch: 'main',
   name: 'social-analytics',
   // cdkDependencies: undefined,  /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
