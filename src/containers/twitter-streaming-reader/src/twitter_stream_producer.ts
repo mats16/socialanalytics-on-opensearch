ea8f2d85d3b0f038dcc8b543bf32768cb6802dc7
Kazuki Matsuda <mats.kazuki@gmail.com>
2022-06-28 02:02:13 +0000

    add metadata for xray

--- src/containers/twitter-streaming-reader/src/twitter_stream_producer.ts
+++ src/containers/twitter-streaming-reader/src/twitter_stream_producer.ts
@@ -1,19 +1,46 @@
 import { EventBridgeClient, PutEventsCommand, PutEventsRequestEntry } from '@aws-sdk/client-eventbridge';
 import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
 import * as xray from 'aws-xray-sdk';
+import axios from 'axios';
 import { TwitterApi, ETwitterStreamEvent, Tweetv2FieldsParams, TweetV2SingleStreamResult } from 'twitter-api-v2';
 import { getLogger } from './logger';
 
 xray.enableManualMode();
-const logger = getLogger();
 
 const region = process.env.AWS_REGION;
+const ecsMetadataUri = process.env.ECS_CONTAINER_METADATA_URI_V4;
 const eventBusArn = process.env.EVENT_BUS_ARN;
 const twitterBearerToken: string = process.env.TWITTER_BEARER_TOKEN!;
 const twitterFieldsParams: Partial<Tweetv2FieldsParams> = JSON.parse(process.env.TWITTER_FIELDS_PARAMS || '{}');
 
+const logger = getLogger();
+
 const eventBridge = new EventBridgeClient({ region });
 const sqs = new SQSClient({ region });
+
+const getPluginData = async () => {
+  if (typeof ecsMetadataUri == 'string') {
+    const { data } = await axios.get(ecsMetadataUri);
+    const pluginData = {
+      container: data.DockerName as string,
+      container_id: data.DockerId as string,
+      container_arn: data.ContainerARN as string,
+    };
+    return pluginData;
+  } else {
+    return {};
+  }
+};
+
+const startSegment = (event: TweetV2SingleStreamResult) => {
+  const segment = new xray.Segment('Twitter Stream Producer');
+  segment.origin = 'AWS::ECS::Container';
+  getPluginData()
+    .then(data => segment.addPluginData(data))
+    .catch(err => logger.error(err));
+  segment.addMetadata('id', event.data.id, 'tweet');
+  return segment;
+};
 
 const putEvent = async (event: TweetV2SingleStreamResult, segment: xray.Segment) => {
   const client = xray.captureAWSv3Client(eventBridge, segment);
@@ -78,8 +105,7 @@ export const twitterStreamProducer = async () => {
     // Emitted when a Twitter payload (a tweet or not, given the endpoint).
     ETwitterStreamEvent.Data,
     eventData => {
-      const segment = new xray.Segment('twitter.com');
-      segment.addMetadata('tweet_id', eventData.data.id);
+      const segment = startSegment(eventData);
       publishEvent(eventData, segment).finally(() => {
         segment.close();
       });
