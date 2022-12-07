70a1543927c2335c9846b007d838e5658f1092db
mats16 <mats.kazuki@gmail.com>
2022-12-07 13:51:14 +0900

    upgrade OpenSearch to 2.3

--- src/resources/dashboard.ts
+++ src/resources/dashboard.ts
@@ -25,7 +25,7 @@ export class Dashboard extends Construct {
 
     this.Domain = new Domain(this, 'Domain', {
       domainName,
-      version: opensearch.EngineVersion.openSearch('1.3'),
+      version: opensearch.EngineVersion.openSearch('2.3'),
       enableVersionUpgrade: true,
       zoneAwareness: { availabilityZoneCount: 3 },
       vpc,
